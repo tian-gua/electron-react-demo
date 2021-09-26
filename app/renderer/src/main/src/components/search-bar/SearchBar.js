5ea9dcbf07ebfd59fea435d4c36999df7faa5249
叶浩 <77729052@qq.com>
2021-09-27 00:58:59 +0800

    .

--- app/renderer/src/main/src/components/search-bar/SearchBar.js
+++ app/renderer/src/main/src/components/search-bar/SearchBar.js
@@ -1,6 +1,6 @@
 import '../../global.css'
-import {useState, useRef} from "react";
-import {Button, Col, Divider, Input, Row, Select, message} from 'antd'
+import {useState} from "react";
+import {Button, Col, Divider, Input, Row, Select, message, Form} from 'antd'
 
 const {Option} = Select
 
@@ -46,48 +46,52 @@ function SearchBar(props) {
         setStockData(res)
     }
 
-    const sectorSelector = useRef()
+    const [form] = Form.useForm()
     const clearSearch = async () => {
-        setStockA('a')
-        setStockB('b')
-        setStockC('c')
-        setSector('')
+        form.resetFields()
     }
 
     const selectSector = async (sectorName) => {
+        if (!sectorName) {
+            return
+        }
+
         const res = await ipcRenderer.invoke('query', 'find-sector-stocks', {sectorName})
         setSectorStocks(res)
     }
     return (
         <div style={{height: 80, paddingTop: '20px', paddingLeft: '10px', overflow: 'hidden'}}>
-            <Row gutter={[10, 10]}>
-                <Col span={4}>
-                    <Select style={{width: '100%'}} placeholder="板块"
-                            defaultValue={sector} onChange={selectSector}>
-                        {sectorOptions}
-                    </Select>
-                </Col>
-                <Col span={4}>
-                    <Select style={{width: '100%'}} placeholder="股票A" onChange={setStockA}>
-                        {stockOptions}
-                    </Select>
-                </Col>
-                <Col span={4}>
-                    <Select style={{width: '100%'}} placeholder="股票B" onChange={setStockB}>
-                        {stockOptions}
-                    </Select>
-                </Col>
-                <Col span={4}>
-                    <Select style={{width: '100%'}} placeholder="股票C" onChange={setStockC}>
-                        {stockOptions}
-                    </Select>
-                </Col>
-                <Col span={8}>
-                    <Button size="large" type="primary" shape="round" style={{marginRight: 10}}
-                            onClick={quickSearch}>快速比较</Button>
-                    <Button size="large" type="danger" shape="round" onClick={clearSearch}>清空</Button>
-                </Col>
-            </Row>
+            <Form form={form}>
+                <Row gutter={[10, 10]}>
+                    <Col span={4}>
+                        <Form.Item name='sector'>
+                            <Select style={{width: '100%'}} placeholder="板块" onChange={selectSector} allowClear={true}>
+                                {sectorOptions}
+                            </Select>
+                        </Form.Item>
+                    </Col>
+                    <Col span={4}>
+                        <Select style={{width: '100%'}} placeholder="股票A" onChange={setStockA}>
+                            {stockOptions}
+                        </Select>
+                    </Col>
+                    <Col span={4}>
+                        <Select style={{width: '100%'}} placeholder="股票B" onChange={setStockB}>
+                            {stockOptions}
+                        </Select>
+                    </Col>
+                    <Col span={4}>
+                        <Select style={{width: '100%'}} placeholder="股票C" onChange={setStockC}>
+                            {stockOptions}
+                        </Select>
+                    </Col>
+                    <Col span={8}>
+                        <Button size="large" type="primary" shape="round" style={{marginRight: 10}}
+                                onClick={quickSearch}>快速比较</Button>
+                        <Button size="large" type="danger" shape="round" onClick={clearSearch}>清空</Button>
+                    </Col>
+                </Row>
+            </Form>
             <Divider/>
         </div>
     );
