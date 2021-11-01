ff57b36f6c06663ded89fdfee913a2754d6ca82e
叶浩 <77729052@qq.com>
2021-11-01 20:42:50 +0800

    .

--- app/main/index.js
+++ app/main/index.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, dialog, BrowserWindow} = require('electron')
 const isDev = require('electron-is-dev')
 const path = require('path')
 const handleIPC = require('./ipc')
@@ -13,13 +13,29 @@ app.on('ready', () => {
             contextIsolation: false
         }
     })
-    if (!isDev) {
-        win.loadURL('http://localhost:3000');
-    } else {
-        const url = path.resolve(__dirname, '../renderer/src/main/build/index.html')
-        console.log(url)
-        win.loadFile(url).then(res => console.log(res)).catch(error => console.error(error))
-    }
+
+    // const url = path.resolve(__dirname, 'static/index.html')
+    const url = "../../page/index.html"
+    // dialog.showMessageBox(win, {
+    //     type: 'info',
+    //     title: '提示',
+    //     message: url
+    // })
+    win.loadFile(url).then(res => console.log(res)).catch(error => console.error(error))
+
+    // if (isDev) {
+    //     win.loadURL('http://localhost:3000');
+    // } else {
+    //     // const url = path.resolve(__dirname, '../renderer/src/main/build/index.html')
+    //     const url = path.resolve(__dirname, 'static/index.html')
+    //     dialog.showMessageBox(win,{
+    //         message: url,
+    //         type: 'info',
+    //         title: '提示'
+    //     })
+    //     console.log(url)
+    //     win.loadFile(url).then(res => console.log(res)).catch(error => console.error(error))
+    // }
 
     handleIPC(win)
 })
